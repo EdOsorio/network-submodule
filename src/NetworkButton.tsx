a960a94ccbfd7c2052a01c1f4b88f6e01997466d
EdOsorio <106685409+EdOsorio@users.noreply.github.com>
2024-03-15 14:29:30 -0500

    Update NetworkButton.tsx

--- src/NetworkButton.tsx
+++ src/NetworkButton.tsx
@@ -2,7 +2,7 @@ interface props {
   onClick: () => void;
 }
 const NetworkButon = ({ onClick }: props) => {
-  return <button onClick={onClick}>Network Button v2</button>;
+  return <button onClick={onClick}>Network Button v4</button>;
 };
 
 export default NetworkButon;
